cb98e4e822379a189fe513f6d7f4ced6744e834b
Gabriel Rodrigues <jipacoding@gmail.com>
2021-07-18 11:24:52 -0400

    feat(utils-path): path_sort: initialSortFN

--- helpers/utils/path/src/path_sort.ts
+++ helpers/utils/path/src/path_sort.ts
@@ -3,5 +3,5 @@ import { path_cmp } from "./path_cmp";
 const locale_cmp = (a: string, b: string) =>
   a.localeCompare(b, "en", { sensitivity: "base" });
 
-export const path_sort = (pathList: string[]) =>
-  pathList.sort(locale_cmp).sort(path_cmp);
+export const path_sort = (paths: string[], initialSortFN = locale_cmp) =>
+  paths.sort(initialSortFN).sort(path_cmp);
