7d5661fbacf10cc186a0391cfee4a4ca79ec49b2
Gabriel Rodrigues <jipacoding@gmail.com>
2021-07-18 11:22:54 -0400

    test(utils-path): path_sort: fix typo

--- helpers/utils/path/src/path_sort.test.ts
+++ helpers/utils/path/src/path_sort.test.ts
@@ -3,7 +3,7 @@ import { path_sort } from "./path_sort";
 describe("path_sort", () => {
   it("file should came after directory", () => {
     expect(
-      path_sort(["file_b", "directory_a/", "file_a", "directory_b"])
-    ).toEqual(["directory_a/", "directory_b", "file_a", "file_b"]);
+      path_sort(["file_b", "directory_a/", "file_a", "directory_b/"])
+    ).toEqual(["directory_a/", "directory_b/", "file_a", "file_b"]);
   });
 });
