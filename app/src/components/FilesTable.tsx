3b6246eed76b41d27d02ce74f7bfae08991ec4eb
Gabriel Rodrigues <jipacoding@gmail.com>
2021-07-19 21:43:08 -0400

    feat(app): components: FilesTable

---
+++ app/src/components/FilesTable.tsx
@@ -0,0 +1,60 @@
+import { DataGrid, GridColDef } from "@material-ui/data-grid";
+import { OpenedFilesContext } from "@ufh/react-services/src/contexts/OpenedFilesContext";
+import { OpenedFile } from "@ufh/react-services/src/OpenedFile";
+import { formatDistance } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import filesize from "filesize";
+import React, { memo } from "react";
+import { useContextSelector } from "use-context-selector";
+
+type FilesTableProps = {
+  onSelectFile?: (openedFile: OpenedFile) => void;
+};
+
+const FilesTableColumns: GridColDef[] = [
+  {
+    flex: 1,
+    field: "name",
+    headerName: "Nome",
+  },
+  {
+    width: 190,
+    align: "right",
+    sortable: true,
+    headerAlign: "right",
+    field: "lastModified",
+    headerName: "Última alteração",
+    valueFormatter: (params) =>
+      params.value
+        ? formatDistance(new Date(+params.value), new Date(), { locale: ptBR })
+        : "N/A",
+  },
+  {
+    width: 150,
+    field: "size",
+    align: "right",
+    sortable: true,
+    headerName: "Tamanho",
+    headerAlign: "right",
+    valueFormatter: (a) => filesize(+a.value!, { standard: "iec" }),
+  },
+];
+
+const FilesTable: React.FC<FilesTableProps> = memo(({ onSelectFile }) => {
+  const openedFilesList = useContextSelector(
+    OpenedFilesContext,
+    ({ list }) => list
+  );
+  return (
+    <DataGrid
+      pageSize={5}
+      checkboxSelection
+      rows={openedFilesList}
+      disableSelectionOnClick
+      columns={FilesTableColumns}
+      onRowClick={(e) => void onSelectFile?.(e.row as OpenedFile)}
+    />
+  );
+});
+
+export default FilesTable;
