e3e8882a01df67e69d9194f881c49fcd89fd40a0
Gabriel Rodrigues <jipacoding@gmail.com>
2021-07-27 11:41:51 -0400

    feat(services): contexts: GlobHandlersContext: validList

--- services/src/contexts/GlobHandlersContextProvider.tsx
+++ services/src/contexts/GlobHandlersContextProvider.tsx
@@ -1,5 +1,5 @@
 import { nanoid } from "nanoid";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState, useMemo } from "react";
 import { GlobHandlersContext } from "./GlobHandlersContext";
 import { IGlobHandler } from "../types/IGlobHandler";
 
@@ -9,60 +9,74 @@ const getNewGlobHandler = (): IGlobHandler => ({
   _id: nanoid(),
 });
 
-const filterGlobExplorerList = (globExplorerList: IGlobHandler[]) =>
-  globExplorerList.filter((i, idx) => i.pattern.length > 0 || idx === globExplorerList.length - 1);
+const filterGlobExplorerList = (list: IGlobHandler[]) =>
+  list.filter((i, idx) => i.pattern.length > 0 || idx === list.length - 1);
 
 export const GlobHandlersContextProvider: React.FC = ({ children }) => {
-  const [globHandlerList, setGlobHandlerList] = useState<IGlobHandler[]>([]);
+  const [tmpList, setTmpList] = useState<IGlobHandler[]>([]);
 
-  const addGlobHandler = useCallback((value?: Partial<IGlobHandler>, insertAfterID?: string) => {
-    setGlobHandlerList((_globExplorerList) => {
-      const globExplorerList = filterGlobExplorerList(_globExplorerList);
+  const validList = useMemo(
+    () => tmpList.filter((i) => i.pattern.length > 0),
+    [tmpList]
+  );
 
-      const globHandler: IGlobHandler = {
-        ...getNewGlobHandler(),
-        ...value,
-      };
+  const addGlobHandler = useCallback(
+    (value?: Partial<IGlobHandler>, insertAfterID?: string) => {
+      setTmpList((_globExplorerList) => {
+        const globExplorerList = filterGlobExplorerList(_globExplorerList);
 
-      const startIndex = globExplorerList.findIndex((i) => i._id === insertAfterID);
+        const globHandler: IGlobHandler = {
+          ...getNewGlobHandler(),
+          ...value,
+        };
 
-      const [before, after] =
-        startIndex > -1
-          ? [globExplorerList.slice(0, startIndex + 1), globExplorerList.slice(startIndex + 1)]
-          : [globExplorerList, []];
+        const startIndex = globExplorerList.findIndex(
+          (i) => i._id === insertAfterID
+        );
 
-      return ([] as IGlobHandler[]).concat(before, globHandler, after);
-    });
-  }, []);
+        const [before, after] =
+          startIndex > -1
+            ? [
+                globExplorerList.slice(0, startIndex + 1),
+                globExplorerList.slice(startIndex + 1),
+              ]
+            : [globExplorerList, []];
+
+        return ([] as IGlobHandler[]).concat(before, globHandler, after);
+      });
+    },
+    []
+  );
 
   const updateGlobHandler = useCallback((id: string, value: IGlobHandler) => {
-    setGlobHandlerList((list) => {
+    setTmpList((list) => {
       return list.map((i) => (i._id === id ? value : value));
     });
   }, []);
 
   const removeGlobHandler = useCallback((id: string) => {
-    setGlobHandlerList((list) => list.filter((i) => i._id !== id));
+    setTmpList((list) => list.filter((i) => i._id !== id));
   }, []);
 
   useEffect(() => {
-    if (globHandlerList.length === 0) {
+    if (tmpList.length === 0) {
       addGlobHandler();
-    } else if (globHandlerList.length > 1) {
-      const globExplorerListFiltered = filterGlobExplorerList(globHandlerList);
-      if (globExplorerListFiltered.length !== globHandlerList.length) {
-        setGlobHandlerList(globExplorerListFiltered);
+    } else if (tmpList.length > 1) {
+      const globExplorerListFiltered = filterGlobExplorerList(tmpList);
+      if (globExplorerListFiltered.length !== tmpList.length) {
+        setTmpList(globExplorerListFiltered);
       }
     }
-  }, [globHandlerList, addGlobHandler]);
+  }, [tmpList, addGlobHandler]);
 
   return (
     <GlobHandlersContext.Provider
       children={children}
       value={{
+        validList,
+        list: tmpList,
+        setList: setTmpList,
         updateGlobHandler,
-        list: globHandlerList,
-        setList: setGlobHandlerList,
         addGlobHandler: addGlobHandler,
         removeGlobHandler: removeGlobHandler,
       }}
